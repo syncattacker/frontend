59ddbbe3a37eb48b58a2cf37242edc29e76fe611
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2024-07-28 22:16:38 +0530

    urls changed

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,20 @@
+// import axios from "axios";
+
+// const apiRequest = axios.create({
+//   baseURL: "https://house-zjit.onrender.com/api",
+//   withCredentials: true,
+// });
+
+// export default apiRequest;
+
 import axios from "axios";
 
 const apiRequest = axios.create({
   baseURL: "https://house-zjit.onrender.com/api",
-  withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
 });
 
 export default apiRequest;
+

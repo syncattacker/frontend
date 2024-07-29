27c3369323933d36f4588aa1387648f0123e4b7b
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2024-07-29 16:00:22 +0530

    middleware

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,23 +1,23 @@
-import axios from "axios";
-
-const apiRequest = axios.create({
-  baseURL: "https://house-zjit.onrender.com/api",
-  withCredentials: true,
-});
-
- export default apiRequest;
-
 // import axios from "axios";
 
 // const apiRequest = axios.create({
 //   baseURL: "https://house-zjit.onrender.com/api",
+//   withCredentials: true,
 // });
 
-// apiRequest.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   config.headers.Authorization = token ? `Bearer ${token}` : "";
-//   console.log("Authorization Header:", config.headers.Authorization); // Debugging
-//   return config;
-// });
+//  export default apiRequest;
 
-// export default apiRequest;
+import axios from "axios";
+
+const apiRequest = axios.create({
+  baseURL: "https://house-zjit.onrender.com/api",
+});
+
+apiRequest.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = token ? `Bearer ${token}` : "";
+  console.log("Authorization Header:", config.headers.Authorization); // Debugging
+  return config;
+});
+
+export default apiRequest;
--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -42,13 +42,13 @@ const res = await apiRequest.post('https://house-zjit.onrender.com/api/auth/logi
    username,
    password,
    }
-  , {
-    headers: {
-      'Access-Control-Allow-Origin': 'https://estate-housing-8eb9a.web.app',
-      'Access-Control-Allow-Credentials' : true,
+  // , {
+  //   headers: {
+  //     'Access-Control-Allow-Origin': 'https://estate-housing-8eb9a.web.app',
+  //     'Access-Control-Allow-Credentials' : true,
 
-    }
-  }
+  //   }
+  // }
 )
 
       updateUser(res.data)
